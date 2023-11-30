e42be473729af49799aa3e4b95306cf41061ae92
sidikh-smi <skhaliff@student.1337.ma>
2023-11-30 21:05:31 +0100

    ...

--- inception/srcs/requirements/wordpress/conf/conf.sh
+++ inception/srcs/requirements/wordpress/conf/conf.sh
@@ -8,31 +8,21 @@ mkdir -p /var/wwww/wordpress
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
+
 mv wp-cli.phar  /usr/local/bin/wp
 
 cd /var/wwww/wordpress
 
 wp core download --allow-root
 
-sleep 10
-
-# mv wp-config-sample.php config.php
-# echo "hello" > config.php
-
-# khalifa_wordpress config create	--allow-root \
-# --dbname=$SQL_DATABASE \
-# --dbuser=$SQL_USER \
-# --dbpass=$SQL_PASSWORD\
+sleep 5
 
 wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --allow-root
 
-wp core install --url=$DOMAINE_NAME --title="Inception" --admin_user=$WP_ADMIN \
---admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
+wp core install --url=$DOMAINE_NAME --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
 
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root 
 
 mkdir -p /run/php
+
 exec /usr/sbin/php-fpm7.4 -F
-
-
-# wp core install --url=$DOMAIN_NAME
